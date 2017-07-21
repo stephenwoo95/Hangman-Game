d52114b1e03de4a9a3d0c0ea00fd104dff2aed29
Stephen Woo <stephenwoo95@gmail.com>
2017-07-20 23:44:43 -0700

    need to fix buttons. All styling done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,87 @@
 
+// Initialize variables
+
 var lives = 6;
 var wordBank = ["PLUMBUS","SQUANCH", "BIRDPERSON", " MR MEESEEKS", "PRINCIPAL VAGINA", "SCARY TERRY", "GET SCHWIFTY", "WUBBA LUBBA DUB DUB", "GAZORPAZORP", "BLIPS AND CHITZ", "ANTS IN MY EYES JOHNSON", "LIL BITS","MR POOPYBUTTHOLE", "STEALY", "SNOWBALL", "RICK", "MORTY", "SUMMER", "JERRY", "BETH", "TINY RICK", "TINYVERSE", "MINIVERSE", "MICROVERSE"];
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var wins = 0;
 var losses = 0;
-var gameStart = true;
-var gameOver = false;
+var gameOver = true;
+var target;
+var shadow = [];
 
 (function(){
+	var guessScreen = document.getElementById("wordGuess");
+	var stats = document.getElementById("lives-wins-losses");
 
-		for(var i=0;i<letters.length;i++){
-			var letterBtn = document.createElement("button");
-			letterBtn.setAttribute("class", "letter-button letter letter-button-color");
-			letterBtn.setAttribute("data-letter", letters[i]);
-			letterBtn.innerHTML=letters[i];
-			var ltrbnk = document.getElementById("letterBank"); 
-			ltrbnk.appendChild(letterBtn);
-		}
+	if(wordBank.length !== 0){
+		if(gameOver){
+			//reset lives & wordguess innerhtml
+			lives = 6;
+			guessScreen.innerHTML = "";
 
-		if(gameStart){
+			//reprint buttons
+			for(var i=0;i<letters.length;i++){
+				var letterBtn = document.createElement("button");
+				letterBtn.setAttribute("class", "letter");
+				letterBtn.setAttribute("dataLetter", letters[i]);
+				letterBtn.setAttribute("onclick", "chooseLetter()");
+				letterBtn.innerHTML=letters[i];
+				var ltrbnk = document.getElementById("letterBank"); 
+				ltrbnk.appendChild(letterBtn);
+			}
+
+			//reset target word
 			var index=Math.floor(Math.random()*wordBank.length);
-			var target = wordBank[index];
+			target = wordBank[index];
 			wordBank.splice(index,1);
+
+			//reset and print shadow of target
 			for(var i=0;i<target.length;i++){
 				if(target[i]!=" "){
-					document.getElementById("wordGuess").innerHTML+=("_ ");
+					shadow.push("_ ");
+					guessScreen.innerHTML+="_ ";
 				}
 				else{
-					document.getElementById("wordGuess").innerHTML+=("&emsp;");
+					shadow.push("&emsp;");
+					guessScreen.innerHTML+="&emsp;";
 				}
 			}
+
+			//print out stats
+			stats.innerHTML = ("wins: " + wins + "&emsp; losses: " + losses + "&emsp; lives: " + lives); 
+
+			gameOver = false;
 		}
 
-		while(!gameOver){
-			document.getElementByClassName("letter-button").on("click",function(){
-				var index=target.indexOf(this.data-letter);
+		document.getElementByTagName("button").on("click",function(){
+			var index=target.indexOf(this.dataLetter);
+			if(index !== -1){
 				while(index != -1){
-
+					shadow[index] = this.dataLetter + " ";
+					target[index] = "*";
+					index = target.indexOf(this.dataLetter);
 				}
-
-				this.disabled = "true";
-			});
-
-		}
+				guessScreen.innerHTML = "";
+				for(var i=0;i<shadow.length;i++){
+					guessScreen.innerhtml+=shadow[i];
+				}
+				if(shadow.indexOf("_ ")==-1){
+					wins++;
+					gameOver = true;
+				}
+			}
+			else{
+				lives--;
+				if(lives === 0){
+					losses++;
+					gameOver = true;
+				}
+			}
+			this.style.color = "darkred";
+			this.disabled = "true";
+		});
+	}
 
 })();
 
